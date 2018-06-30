113802831ed2b172d2a9c2c15bf4ae81ce2d1ab8
Slađan Ristić <sladiri@users.noreply.github.com>
2018-06-30 11:49:05 +0200

    ClientApp: add assertions

--- src/client.mjs
+++ src/client.mjs
@@ -5,10 +5,33 @@ import {
 } from "./control/client";
 
 export const ClientApp = async options => {
+    console.assert(
+        window && window instanceof Window,
+        "ClientApp: window && window instanceof Window",
+    );
+    console.assert(
+        typeof options === "object" && options !== null,
+        "ClientApp: typeof options === 'object' && options !== null",
+    );
     const { rootElement, state = Object.create(null) } = options;
-    let serverSideRender = !!window["dispatcher"];
-    if (window["dispatcher"]) {
-        serverSideRender = true;
+    console.assert(
+        rootElement instanceof HTMLElement,
+        "ClientApp: rootElement instanceof HTMLElement",
+    );
+    console.assert(
+        typeof state === "object" && state !== null,
+        "ClientApp: typeof state === 'object' && state !== null",
+    );
+    let serverSideRender = !!window.dispatcher;
+    if (serverSideRender) {
+        console.assert(
+            typeof window.dispatcher === "object" && window.dispatcher !== null,
+            "ClientApp: typeof window.dispatcher === 'object' && window.dispatcher !== null",
+        );
+        console.assert(
+            Array.isArray(window.dispatcher.toReplay),
+            "ClientApp: Array.isArray(window.dispatcher.toReplay)",
+        );
     } else {
         window["dispatcher"] = { toReplay: [] };
     }
